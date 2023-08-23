992ba5361fe04252e4a1d10efd98002d0cc52b2a
AJU <aulcena@gmail.com>
2023-08-23 15:28:02 -0400

    added users table

--- db/migrate/20230822215358_create_users.rb
+++ db/migrate/20230822215358_create_users.rb
@@ -1,12 +1,10 @@
 class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
-      t.string :username, null: false
-      t.string :password_digest, null: true
-      t.string :session_token, null: true
+      t.string :username, null: false, index: {unique: true}
+      t.string :password_digest, null: false
+      t.string :session_token, null: false, index: {unique: true}
       t.timestamps
-
-      add_index :users, :username
     end
   end
 end
