e02386c0a8c657b996b759919bc14afb7e6d319f
AJU <aulcena@gmail.com>
2023-08-25 00:30:27 -0400

    crediantials fixed

--- app/models/user.rb
+++ app/models/user.rb
@@ -28,7 +28,7 @@ class User < ApplicationRecord
     def self.find_by_credentials(username, password)
         user = User.find_by(username: username)
         return nil if user.nil?
-        user.is_password?(password) ? user : nil
+        user && user.is_password?(password) ? user : nil
     end
 
     def reset_session_token!
